3416c4a898a3b47a42a84e64d980bfa9b8ff3f25
Lau Homeres <laurencehomeres0@gmail.com>
2024-11-04 17:40:32 +0800

    Initial commit

---
+++ classsubject.js
@@ -0,0 +1,11 @@
+// ClassSubject.js
+
+
+let subjectTitle = "Data strustures and algorithms"; 
+let classSchedule = "Tuesday & Thursday, 8:00 AM - 10:00 AM";
+let classroom = "Comlab B";
+let classInstructor = "Vitruvius john Barayuga"; 
+let studentName = "Lurence Homerez"; 
+
+// Log class information to the console in the specified format
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
